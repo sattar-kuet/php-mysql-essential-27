c3c477bd791d4956944ca3b24429e35816410cff
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 15:00:46 +0600

    first commit

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+
+
+<form method="post" action="process.php">
+	<input type="text" name="first_name" placeholder="Type first name here">
+	<input type="text" name="last_name" placeholder="Type last name here">
+	<input type="text" name="email" placeholder="Type email here">
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
